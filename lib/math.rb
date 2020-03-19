2ef415ab2abc7049a0cbd6ca2748c5bf9b8b027b
Injae Lee <ilee.injae@gmail.com>
2020-03-19 19:53:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  addition = num1 + numb2
+  puts addition
 end
 
 def subtraction(num1, num2)
-
+    subtraction = num1 - num2
+    puts subtraction
 end
 
 def division(num1, num2)
-
+ division = num1 / num2
+ puts division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
+  puts multiplication 
 end
 
 def modulo(num1, num2)
-
+  modulo = num1 % num2
+  puts modulo
 end
 
 def square_root(num)
-
+  square_root = num **
+  puts square_root
 end
